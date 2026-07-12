070a3a4c10b20b28db724679dab8feafaabce572
Keerthi D C <dckeerthi2004@gmail.com>
2026-07-12 21:01:04 +0700

    modified the README.md

--- online-library/src/App.jsx
+++ online-library/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
-import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
---
+++ online-library/src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const activeStyle = {
+    fontWeight: "bold",
+    color: "#ff6b6b",
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">Online Library</div>
+
+      <ul className="navbar__links">
+        <li>
+          <NavLink
+            to="/"
+            end
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Home
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/browse"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Browse
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/add"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Add Book
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
